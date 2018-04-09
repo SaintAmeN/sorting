46137468e4563a93e6a1aa69361c6870cc678f48
Paweł Recław <pawel.reclaw@gmail.com>
2018-04-09 19:27:14 +0200

    Initial.

---
+++ src/main/java/com/sda/sorting/Main.java
@@ -0,0 +1,14 @@
+package com.sda.sorting;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] tablica = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//        int[] tablica = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+        System.out.println("Przed " + Arrays.toString(tablica));
+        BubbleSort.sort(tablica);
+        System.out.println("Po    " + Arrays.toString(tablica));
+    }
+}
